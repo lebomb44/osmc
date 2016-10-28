562993c877867db359214f4ed4fca8df87d12ca3
LeBomb <cambon.olivier@gmail.com>
2016-10-28 23:05:43 +0200

    Add initial files

---
+++ owncloud/config.php
@@ -0,0 +1,30 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oca72ed23ae3',
+  'passwordsalt' => 'd2afa797eb46c73f7acb996de201f2',
+  'secret' => '2fb3594dfe16a99a88e56f83ccb68e3a34a6e1c4edd552f43340b1427aada62c69a2755fa0fcdcde5dc67eb166db60ce',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.10.5',
+  ),
+  'datadirectory' => '/media/HDD/owncloud',
+  'overwrite.cli.url' => 'http://192.168.10.5:8081/owncloud',
+  'dbtype' => 'mysql',
+  'version' => '7.0.4.2',
+  'appstoreenabled' => false,
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/usr/share/owncloud/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+  ),
+  'dbname' => 'owncloud',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc_lebomb',
+  'dbpassword' => 'a0d156a84d0f7282394c342ed91b0d',
+  'installed' => true,
+);
