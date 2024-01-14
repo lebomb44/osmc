7c32aa0a18ff116fb38b01f0a6ab2eb25e80d0de
LeBomb <cambon.olivier@gmail.com>
2024-01-14 19:14:35 +0100

    Update rsync_bourdilot.sh

--- rsync_bourdilot.sh
+++ rsync_bourdilot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/bin/rsync -av --progress --append-verify --files-from=/home/osmc/Movies/Bourdilot.txt /home/osmc/Movies/. -e "ssh -p 2205" osmc@bourdilot.ddns.net:/home/osmc/Movies/.
+/usr/bin/rsync -av --progress --append-verify --files-from=/home/osmc/Movies/Bourdilot.txt /home/osmc/Movies/. -e "ssh -p 2205" osmc@bourdilot.tbd.tbd:/home/osmc/Movies/.
 
