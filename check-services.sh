2817c42911375c80faa01f774a0d716821da86f0
LeBomb <cambon.olivier@gmail.com>
2024-06-05 20:57:33 +0200

    Add wireguard check

--- check-services.sh
+++ check-services.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ping1=$(/usr/bin/wget -q -O /dev/stdout --no-cache --timeout=5 --no-check-certificate https://127.0.0.1/ping)
 #echo $ping1
 if [ "pong" != "$ping1" ]; then
@@ -32,3 +34,19 @@ else
     echo "`date`: ShellInABox OK at first check"
 fi
 
+pivpnPROTO=udp
+pivpnPORT=51820
+VPN_PRETTY_NAME="Wireguard"
+VPN_SERVICE=wg-quick@wg0
+
+if /bin/netstat -antu | /bin/grep -wqE "${pivpnPROTO}.*${pivpnPORT}"; then
+  echo -n "`date`: [OK] ${VPN_PRETTY_NAME} is listening "
+  echo "on port ${pivpnPORT}/${pivpnPROTO}"
+else
+  echo "`date`: [ERR] ${VPN_PRETTY_NAME} is not listening"
+  /bin/systemctl restart "${VPN_SERVICE}"
+  echo "`date`: ${VPN_PRETTY_NAME} restarted"
+fi
+
+echo "`date`: Mounting all drives"
+/usr/bin/sudo /bin/mount -a
